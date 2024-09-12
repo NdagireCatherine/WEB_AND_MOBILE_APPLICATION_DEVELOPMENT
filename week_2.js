95ae954c00d9ce7e28b87cdd3e460a56d7a0464d
Ndagire Catherine <tronellahn@gmail.com>
2024-09-13 00:08:11 +0300

    Add files via upload

---
+++ week_2.js
@@ -0,0 +1,30 @@
+console.log("Hello world");
+console.log("Ndagire Catherine");
+console.log("B27015");
+//comand to print
+
+//initializing
+var my_name = "Ndagire Catherine";
+
+//declare a variable
+//the var declares a variable
+var height;
+
+//assign a variable
+height = 3.4;
+console.log(height);
+//always terminate your code;
+
+//let
+let area = 90.5;
+let f_name;
+f_name = "Cathy";
+//works the same way as var
+
+//const
+const pie = 3.14;
+//this value can't be changed when assigning the same variable
+
+//concactinating
+console.log(my_name,"-",height)
+console.log("First name",f_name)
