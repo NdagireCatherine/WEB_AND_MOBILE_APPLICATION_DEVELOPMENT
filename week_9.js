62a156bd797e1d84ce4155ad6d98ab32e6d5fbf9
Ndagire Catherine <tronellahn@gmail.com>
2024-11-01 23:47:33 +0300

    commit

--- week_9.js
+++ week_9.js
@@ -1,14 +1,23 @@
+//approxinating
 const checkApprox = (num_1, num_2, epsilon = 0.004) => {
     return Math.abs(num_1-num_2) < epsilon;
 }
-
 console.log(checkApprox(0.003, 0.03));
 
-
+//calculating current milliseconds
 let currentMilliseconds = Date.now();
-console.log(currentMilliseconds)
+console.log(currentMilliseconds);
 
-
+//spliting words
 let text = "Hey How are you";
 let result = text.split(" ");
 console.log(result);
+
+//playing items in an array
+const person = {
+    name: 'Catherine',
+    age: 20,
+    job: 'Data Scientist'
+};
+const values = Object.values(person);
+console.log(values)
