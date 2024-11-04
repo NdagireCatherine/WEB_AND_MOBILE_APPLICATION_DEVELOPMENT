350623a0bad1dc6e3560fb0fd697306de66c32b1
Ndagire Catherine <tronellahn@gmail.com>
2024-11-04 23:27:28 +0300

    commit

--- week_9.js
+++ week_9.js
@@ -45,3 +45,18 @@ for(let i=1; i<=number; i++){
     n1=n2;
     n2=nextTerm
 }
+
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+const num3 = parseFloat(prompt("Enter third number: "));
+
+if(num1 >= num2 && num1 >= num3){
+    largest = num1;
+}
+else if (num2 >= num1 && num2 >= num3){
+    largest = num2;
+}
+else{
+    largest = num3;
+}
+console.log("The largest number is" + largest)
